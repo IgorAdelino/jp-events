16e32771ea8286203dce9de7094927cf7c201a10
IgorAdelino <igoradelino@gmail.com>
2024-06-10 13:35:18 -0300

    fix: image

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -11,7 +11,7 @@ type CollectionProps = {
   limit: number
   totalPages?: number
   urlParamName?: string
-  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_events'
+  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
 }
 
 const Collection = ({
